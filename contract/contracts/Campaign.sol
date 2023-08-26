657e2cb62393c6a76e3a1880aaf361d9e2618dba
bugattie <m.mohsinbarkaati@gmail.com>
2023-08-26 18:48:54 +0500

    working on the contract

--- contract/contracts/Campaign.sol
+++ contract/contracts/Campaign.sol
@@ -3,5 +3,46 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
 
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
+
+    constructor(uint minimum) {
+        manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    modifier restrictedToManager() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers.push(msg.sender);
+    }
+
+    function createRequest(
+        string memory description,
+        uint value,
+        address recipient
+    ) public restrictedToManager {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
+    }
 }
