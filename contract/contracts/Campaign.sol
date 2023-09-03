7c9d5c24b57fc78145d0e6269eac97e12758b5f1
bugattie <m.mohsinbarkaati@gmail.com>
2023-09-03 23:50:01 +0500

    code refactored to mapping, finalize request function

--- contract/contracts/Campaign.sol
+++ contract/contracts/Campaign.sol
@@ -8,12 +8,15 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
 
     constructor(uint minimum) {
         manager = msg.sender;
@@ -28,7 +31,8 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(
@@ -36,13 +40,34 @@ contract Campaign {
         uint value,
         address recipient
     ) public restrictedToManager {
-        Request memory newRequest = Request({
-            description: description,
-            value: value,
-            recipient: recipient,
-            complete: false
-        });
+        Request storage newRequest = requests.push();
 
-        requests.push(newRequest);
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+        newRequest.approvalCount = 0;
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restrictedToManager {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        address payable playerAddress = payable(request.recipient);
+        playerAddress.transfer(request.value);
+
+        request.complete = true;
     }
 }
